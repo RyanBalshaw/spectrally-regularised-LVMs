9849cf79b360aec49031d013c8216ac30fd1b80f
RyanBalshaw <ryanbalshaw81@gmail.com>
2023-04-30 15:10:44 +0200

    Added logcosh function.

---
+++ spectrally_constrained_ICA/negentropy_approximators.py
@@ -0,0 +1,228 @@
+"""
+The negentropy approximation functions for the FastICA methods.
+"""
+import numpy as np
+
+
+class general_object(object):
+    """
+    An object that implements the first derivative, second derivative and
+    gamma functions of the XXXX (fill in here) function. These functions are used in the
+    negentropy calculation for FastICA.
+
+    Methods
+    -------
+    function(u = float)
+        Return the function form of G(u) for the XXXX function
+
+    first_derivative(u = float)
+        Return the function form of the first derivative g(u) for the XXXX function
+
+    second_derivative(u = float)
+        Return the function form of the second derivativeg'(u) for the XXXX function
+
+    gamma(u = float)
+        Return the function form of ratio g'(u)/g(u) for the XXXX function
+    """
+
+    def function(self, u: float):
+        """
+        This method implements the functional form of G(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through G(u)
+
+        Returns
+        -------
+            float: The computation of G(u)
+        """
+
+        return None
+
+    def first_derivative(self, u: float):
+        """
+        This method implements the first derivative of G(.) for g(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through g(u)
+
+        Returns
+        -------
+            float: The computation of g(u)
+        """
+
+        return None
+
+    def second_derivative(self, u: float):
+        """
+        This method implements the second derivative of G(.) for g'(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through g'(u)
+
+        Returns
+        -------
+            float: The computation of g'(u)
+        """
+
+        return None
+
+    def gamma(self, u):
+        """
+        This method implements the ratio of the second derivative to
+        the first derivative (gamm(u) = g'(u) / g(u))
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through gamma(u)
+
+        Returns
+        -------
+            float: The computation of gamma(u)
+        """
+
+        return None
+
+
+class logcosh_object(object):
+    """
+    An object that implements the first derivative, second derivative and
+    gamma functions of the logcosh function. These functions are used in the
+    negentropy approximation calculation for FastICA.
+
+    Methods
+    -------
+    function(u = float)
+        Return the function form of G(u) for the logcosh function
+
+    first_derivative(u = float)
+        Return the function form of the first derivative g(u) for the logcosh function
+
+    second_derivative(u = float)
+        Return the function form of the second derivativeg'(u) for the logcosh function
+
+    gamma(u = float)
+        Return the function form of ratio g'(u)/g(u) for the logcosh function
+    """
+
+    def __init__(self, a1=None):
+        """
+
+        Parameters
+        ----------
+        a1: float
+            The a1 parameter for the  generalised logcosh function.
+
+        Raises
+        ------
+        ValueError
+            If the a1 value is outside the recommended domain of [1, 2]
+        """
+        if a1 is None:
+            self.a1 = [1]  # [a1 = 1]
+
+        else:
+            if a1 < 1 or a1 > 2:
+                print(
+                    f"a1 parameter ({a1}) in logcosh object is outside [1, 2] bounds."
+                )
+                raise ValueError
+
+            self.a1 = a1  # [a1]
+
+    def function(self, u):
+        """
+        This method implements the functional form of G(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through G(u)
+
+        Returns
+        -------
+            float: The computation of G(u)
+        """
+
+        return (
+            1
+            / (self.a1)
+            * np.log((np.exp(self.a1 * u) + np.exp(-self.a1 * u)) / 2 + 1e-12)
+        )  # 1e-12 for stability
+
+    def first_derivative(self, u):
+        """
+        This method implements the first derivative of G(.) for g(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through g(u)
+
+        Returns
+        -------
+            float: The computation of g(u)
+        """
+        # au = self.a1 * u
+        # e_au = np.exp(au)
+        # e_n_au = np.exp(-au)
+
+        return np.tanh(self.a1 * u)
+        # (e_au - e_n_au) / (
+        # e_au + e_n_au)
+
+    def second_derivative(self, u):
+        """
+        This method implements the second derivative of G(.) for g'(u)
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through g'(u)
+
+        Returns
+        -------
+            float: The computation of g'(u)
+        """
+        d1 = self.first_derivative(self.a1 * u)
+
+        return 1 - self.a1 * d1 * d1
+
+    def gamma(self, u):
+        """
+        This method implements the ratio of the second derivative to
+        the first derivative (gamm(u) = g'(u) / g(u))
+
+        Parameters
+        ----------
+        u: (float): The input value to be fed through gamma(u)
+
+        Returns
+        -------
+            float: The computation of gamma(u)
+        """
+        return self.second_derivative(u) / self.first_derivative(u)
+
+    # I played around with finding the inverse distribution from the
+    # transformed distribution.
+    # Not useful here.
+
+    # def inverse(self, p):
+    #     # p represents the variables mapped to X -> P, we wish to recover X
+    #
+    #     e_p = np.exp(p)
+    #
+    #     return np.array([-1, 1]) * np.log(e_p + np.sqrt(e_p**2 - 1)).reshape(
+    #         -1, 1
+    #     )  # Can have two solutions
+    #
+    # def inverse_derivative(self, p):
+    #
+    #     e_p = np.exp(p)
+    #     e_2p = e_p**2
+    #
+    #     numerator = e_p + e_2p * (e_2p - 1) ** (-1 / 2)
+    #     denominator = e_p + np.sqrt(e_2p - 1)
+    #     deriv = numerator / denominator
+    #
+    #     return np.array([-1, 1]) * deriv.reshape(-1, 1)
