c26177e98eecb2c2e77cc98b23d957eb13a9814a
RyanBalshaw <ryanbalshaw81@gmail.com>
2023-06-09 12:22:06 +0200

    Updated init file for new name.

--- spectrally_regularised_LVMs/__init__.py
+++ spectrally_regularised_LVMs/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2023-present Ryan Balshaw
-"""Spectrally-constrained-LVMs. Train linear LVMs with the addition
- of a spectral constraint with minimal effort."""
+"""Spectrally-regularised-LVMs. Train linear LVMs with the addition
+ of a spectral regularisation term with minimal effort."""
 from .cost_functions import NegentropyCost, SympyCost, UserCost, VarianceCost
 from .helper_methods import (
     BatchSampler,
@@ -10,17 +10,17 @@ from .helper_methods import (
     hankel_matrix,
 )
 from .negen_approx import CubeObject, ExpObject, LogcoshObject, QuadObject
-from .spectral_constraint import SpectralObjective
-from .spectrally_constrained_model import LinearModel
+from .spectral_regulariser import SpectralObjective
+from .spectrally_regularised_model import LinearModel
 
 __author__ = "Ryan Balshaw"
-__version__ = "0.1.3"
+__version__ = "0.1.1"
 __email__ = "ryanbalshaw81@gmail.com"
 __description__ = (
     "Train linear LVMs with the addition "
-    "of a spectral constraint with minimal effort."
+    "of a spectral regularisation term with minimal effort."
 )
-# __uri__ = "http://spectrally-constrained-lvms.readthedocs.io/"
+__uri__ = "http://spectrally-regularised-lvms.readthedocs.io/"
 __all__ = [
     "SpectralObjective",
     "NegentropyCost",
